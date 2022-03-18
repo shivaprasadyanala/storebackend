431cb91456f059992a0c5ea1d8603b1fd2575cac
shivaprasadyanala <shivaprasadyanala@gmail.com>
2022-03-18 12:46:58 +0530

    no echo statement

--- connection.php
+++ connection.php
@@ -19,5 +19,5 @@ $conn = mysqli_connect($servername, $username, $password,$db,$port);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
+// echo "Connected successfully";
 ?>
