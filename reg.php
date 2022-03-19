a56d2c9055361a642be8becd2e34c304da02a7a2
shivaprasadyanala <shivaprasadyanala@gmail.com>
2022-03-19 19:35:40 +0530

    change

--- reg.php
+++ reg.php
@@ -27,14 +27,14 @@ if(isset($_POST['submit']))
       $sql = "INSERT INTO register(first_name,last_name,email,pass,phn_no,Addres,gender,stat,city,pincode) VALUES ('$f_name','$l_name','$email','$stored','$mobile','$address','$gender','$state','$city','$pin')";
        if (mysqli_query($conn, $sql) ) {
         echo "New record has been added successfully !";
-        header('location:./login.php');
+        header('location: /login.php');
         exit();
      } else {
         echo "Error: " . $sql . ":-" . mysqli_error($conn);
       //   header('location:reg.php');
      }
      }else{
-          header('location:./register.php?error=email');
+          header('location: /register.php?error=email');
       //   echo "email already exist";
      }
      
